c1e7ac5351d334fc627731816ab68689972ff850
Johnstone Ananda <johnolwamba@gmail.com>
2021-06-27 00:09:21 -0500

    bold textarea text when checkbox is checked

--- js/decoratemytext.js
+++ js/decoratemytext.js
@@ -10,17 +10,36 @@ window.onload = function () {
     const clickedButton = document.getElementById("decoButton");
     clickedButton.onclick = changeFontOfTextArea;
 
+    // textarea
+    const textArea = document.getElementById("inputTextArea");
+
     function makeAlertCall() { // display an alert
         alert("Hello World!");
     }
 
     function changeFontOfTextArea() { /*changing font size of textarea text*/
-        const textArea = document.getElementById("inputTextArea");
         textArea.style.fontSize = "24pt";
     }
 
     /* Pick checkbox and alert onchange */
     const checkBox = document.getElementById("checkBox");
-    checkBox.onchange = makeAlertCall;
+    // checkBox.onchange = makeAlertCall;
+    checkBox.onchange = checkIfCheckBoxIsOn;
+
+    /* update font weight of textarea onchange of checkbox */
+    function changeFontWeightOfTextArea(shouldBold) {
+        if (shouldBold) {
+            textArea.style.fontWeight = "bold";
+        } else {
+            textArea.style.fontWeight = "normal";
+        }
+    }
+
+    //detect checkbox change
+    function checkIfCheckBoxIsOn() {
+        changeFontWeightOfTextArea(checkBox.checked);
+    }
+
+    
 
 };
