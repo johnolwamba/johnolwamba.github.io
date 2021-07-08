a3e0966128652f4ee9838d93578612a42a35457a
Johnstone Ananda <johnolwamba@gmail.com>
2021-07-08 16:50:30 -0500

    done math computations on question 3

---
+++ nodejsintro/my_math.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+const pi = 3.14
+
+module.exports = {
+    'add': add,
+    'subtract': subtract,
+    'multiply': multiply,
+    'divide': divide,
+    'pi': pi
+}
