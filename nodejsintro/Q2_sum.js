7dbd4717d6ccd062184287918b41b2702288da8b
Johnstone Ananda <johnolwamba@gmail.com>
2021-07-08 15:48:45 -0500

    done question 2 on sum

---
+++ nodejsintro/Q2_sum.js
@@ -0,0 +1,19 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function getNumber() {
+    readline.question("Enter a number, or the word stop to get the sum: ", input => {
+        if (isNaN(input) || input.toLowerCase() === "stop") {
+            console.log(`Your total sum is: ${sum}`);
+            readline.close();
+        } else {clearInterval()
+            sum += parseInt(input);
+            getNumber();
+        }
+    });
+}
+
+let sum = 0;
+getNumber();
