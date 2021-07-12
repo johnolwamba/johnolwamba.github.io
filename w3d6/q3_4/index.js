02042cef0ba6c781a3b5714ffc14b91f2a6763f1
Johnstone Ananda <johnolwamba@gmail.com>
2021-07-11 21:13:25 -0500

    sets footer and header

---
+++ w3d6/q3_4/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+let Product = require("./models/product");
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "view"));
+app.use(express.urlencoded({extended: false}));
+
+function buildProductArray() {
+    Product.removeAll();
+    (new Product(1, "Ugali", 200, "Maize flour paste")).save();
+    (new Product(2, "Sukuma Wiki", 140, "Kales")).save();
+    (new Product(3, "Cabbage", 120, "Cooked with ugali")).save();
+    (new Product(4, "Cereals", 50, "Protein rich")).save();
+    (new Product(5, "Wheat", 30, "Body building feeds")).save();
+    return Product.getAll();
+}
+
+app.get("/", (req, res) => {
+    const products = buildProductArray();
+    res.render("index", {
+        products: products,
+        pageTitle: 'Products'
+    });
+});
+
+app.post("/addToCart", (req, res) => {
+    const {productId} = req.body;
+});
+
+function buildShoppingCart() {
+    return [{
+        quantity: 1,
+        product: new Product(1, "Ugali", 200, "Maize flour paste")
+    }, {
+        quantity: 2,
+        product: new Product(2, "Sukuma Wiki", 140, "Kales")
+    }, {
+        quantity: 1,
+        product: new Product(3, "Cabbage", 120, "Cooked with ugali")
+    }, {
+        quantity: 3,
+        product: new Product(4, "Cereals", 50, "Protein rich")
+    }
+    ];
+}
+
+app.get("/shoppingcart", (req, res) => {
+    const list = buildShoppingCart();
+
+    let total = 0;
+    list.forEach(value => {
+        total += (value.quantity * value.product.price);
+    });
+
+    res.render("shoppingcart", {
+        shoppinglist: list,
+        total: total,
+        pageTitle: 'Shopping Cart'
+    });
+});
+
+app.listen(3000);
---
+++ w3d6/q3_4/models/product.js
@@ -0,0 +1,22 @@
+const products = [];
+
+module.exports = class Product {
+    constructor(id, name, price, description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    save() {
+        products.push(this);
+    }
+
+    static getAll() {
+        return products;
+    }
+
+    static removeAll() {
+        products.length = 0;
+    }
+}
